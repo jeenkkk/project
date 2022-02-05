4159362be4c100c64244bd06073fccc7dbd4572c
jeenkkk <saymynameje3n@gmail.com>
2022-02-05 23:31:26 +0700

    main

--- js/search.js
+++ js/search.js
@@ -5,9 +5,23 @@ function searchbyhide() {
     table = document.getElementById("tb");
     tr = table.getElementsByTagName("tr");
   
-    // loop table for hide items that doesn't match in input
+    // loop table for hide items that doesn't match in input by name
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txt = td.textContent || td.innerText;
+        if (txt.toLowerCase().indexOf(filter) >= 0) {
+            console.log(txt.toLowerCase().indexOf(filter));
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+
+    // search by price
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
       if (td) {
         txt = td.textContent || td.innerText;
         if (txt.toLowerCase().indexOf(filter) >= 0) {
